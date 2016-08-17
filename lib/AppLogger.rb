a916770a2c3bd9cdfe300943df1b2ec7b3fee31b
Mauricio Giraldo <mgiraldo@gmail.com>
2016-08-17 11:15:42 -0400

    Simplified Logger for less redundant formatters

--- lib/AppLogger.rb
+++ lib/AppLogger.rb
@@ -6,22 +6,22 @@ class AppLogger
 
   def self.info(message, clazz='N/A', method='N/A')
     AppLogger.get_log_time_format
-    @@logger.info(" - class=#{clazz} - method=#{method} - message=" + message)
+    @@logger.info(AppLogger.format_output(clazz, method, message))
   end
 
   def self.debug(message, clazz='N/A', method='N/A')
     AppLogger.get_log_time_format
-    @@logger.debug(" - class=#{clazz} - method=#{method} - message=" + message)
+    @@logger.debug(AppLogger.format_output(clazz, method, message))
   end
 
   def self.error(message, clazz='N/A', method='N/A')
     AppLogger.get_log_time_format
-    @@logger.error(" - class=#{clazz} - method=#{method} - message=" + message)
+    @@logger.error(AppLogger.format_output(clazz, method, message))
   end
 
   def self.warning(message, clazz='N/A', method='N/A')
     AppLogger.get_log_time_format
-    @@logger.warn(" - class=#{clazz} - method=#{method} - message=" + message)
+    @@logger.warn(AppLogger.format_output(clazz, method, message))
   end
 
   def self.get_log_time_format
@@ -29,6 +29,10 @@ class AppLogger
       date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
       "[#{date_format}] - (#{program_name}) #{'[%5s]'% severity.upcase} - #{msg.inspect}\n"
     end
+  end
+
+  def self.format_output(clazz, method, message)
+    " - class=#{clazz} - method=#{method} - message=" + message
   end
 
 end
